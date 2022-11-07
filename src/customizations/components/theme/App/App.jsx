08964d4a7063b20eebe346532476f275c87b17ed
nileshgulia1 <nileshgulia@gmail.com>
2022-11-07 15:53:09 +0530

    fix slots actions

--- src/customizations/components/theme/App/App.jsx
+++ src/customizations/components/theme/App/App.jsx
@@ -39,8 +39,9 @@ import {
   getNavigation,
   getTypes,
   getWorkflow,
-  getSlots,
 } from '@plone/volto/actions';
+
+import { getSlots } from '@eeacms/volto-slots/actions';
 
 import clearSVG from '@plone/volto/icons/clear.svg';
 import MultilingualRedirector from '@plone/volto/components/theme/MultilingualRedirector/MultilingualRedirector';
